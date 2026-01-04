aff1ec9d4bfe9ffff636b6d9bdd83d0defef1ccd
KH.Bidyut <khbidyut31@gmail.com>
2026-01-05 01:29:01 +0600

    learn demonstrate class with constructor

--- src/hi.ts
+++ src/hi.ts
@@ -65,3 +65,17 @@ let myCar: Car = {
     year: 2020
 };
 console.log(myCar);
+
+// step 10: demonstrate class with constructor
+class Animal {
+    name: string;   
+    constructor(name: string) {
+        this.name = name;
+    }
+    speak() {
+        console.log(`${this.name} makes a noise.`);
+    }               
+}
+
+let dog = new Animal("Dog");
+dog.speak();
